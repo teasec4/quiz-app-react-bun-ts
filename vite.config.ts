6af7fd767eb9da7348caba04d7dbcaad7b7ec4a8
Kovalev Maksim <teasec4@yandex.ru>
2026-02-26 15:35:03 +0800

    fix: switch to vite build and fix css paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/quiz-app-react-bun-ts/',
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: 'esbuild',
+  }
 })
